8edc7ae08764a1338759871a76a9d04e4e095229
Alan Chandler <alan@chandlerfamily.org.uk>
2021-01-08 12:33:32 +0000

    Removed unwanted module

--- client/modules/formatter.js
+++
@@ -1,8 +0,0 @@
-export default (amount) => {
-  let result = '';
-  if (amount < 0) result +='-';
-  const absamount = Math.abs(amount);
-  const cents = absamount % 100;
-  const dollars = Math.floor(absamount/100);
-  result = dollars.toS
-}
