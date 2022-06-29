eb008c098b92ed17970a91d75916ece20be6050d
Alan Chandler <alan@chandlerfamily.org.uk>
2022-06-29 17:51:19 +0100

    Fix typo in account transaction

--- client/elements/account-transaction.js
+++ client/elements/account-transaction.js
@@ -391,7 +391,7 @@ class AccountTransaction extends LitElement {
     }
     if (changed.has('readonly')) {
       const wrapper = this.shadowRoot.querySelector('#wrapper');
-      if (wrapperrsy) {
+      if (wrapper) {
         if (this.readonly) {
           let tooltip;
           if (this.dst !== null && this.src !== null) {
