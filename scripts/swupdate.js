1493d59973cf91bed0880ce9435e3d600fe52835
Alan Chandler <alan@chandlerfamily.org.uk>
2024-05-11 13:29:07 +0100

    Whitespace adjustment

--- scripts/swupdate.js
+++ scripts/swupdate.js
@@ -19,41 +19,41 @@ import fs from 'node:fs/promises';
 import {fileURLToPath } from 'node:url';
 
 async function update(fn, s, t) {
-    const filename = fn;
-    const str = s;
-    const token = t;
-    const result = await fs.readFile(filename, 'utf8');
-    let madeChange = false;
-    let lines;
-    if (result) {
-      lines = result.toString().split("\n");
-      while (lines[lines.length - 1] === '') {
-        lines.pop(); //get rid of dummy lines at the end
-      }
-      let found = false;
-      for (let i = 0; i < lines.length; i++) {
-        if (lines[i].substring(0, str.length) === str) {
-          if (lines[i] !== `${str} '${token.toString()}'`) {
-            lines[i] = `${str} '${token.toString()}'`; //replace with our new token
-            madeChange = true;
-          }
-          found = true;
-          break;
+  const filename = fn;
+  const str = s;
+  const token = t;
+  const result = await fs.readFile(filename, 'utf8');
+  let madeChange = false;
+  let lines;
+  if (result) {
+    lines = result.toString().split("\n");
+    while (lines[lines.length - 1] === '') {
+      lines.pop(); //get rid of dummy lines at the end
+    }
+    let found = false;
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i].substring(0, str.length) === str) {
+        if (lines[i] !== `${str} '${token.toString()}'`) {
+          lines[i] = `${str} '${token.toString()}'`; //replace with our new token
+          madeChange = true;
         }
+        found = true;
+        break;
       }
-      if (!found) {
-        lines.push(`${str} '${token.toString()}'`); //not found so append
-        lines.push(''); //just add this so file ends just after the last entry
-        madeChange = true;
-      }
-    } else {
-      lines = [];
-      lines.push(`${str} '${token.toString()}'`);
+    }
+    if (!found) {
+      lines.push(`${str} '${token.toString()}'`); //not found so append
       lines.push(''); //just add this so file ends just after the last entry
       madeChange = true;
     }
-    if (madeChange) await fs.writeFile(filename, lines.join("\n"))
+  } else {
+    lines = [];
+    lines.push(`${str} '${token.toString()}'`);
+    lines.push(''); //just add this so file ends just after the last entry
+    madeChange = true;
   }
+  if (madeChange) await fs.writeFile(filename, lines.join("\n"))
+}
   
 
 
