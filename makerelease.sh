900c40bc3d72080a0ca06a2428e4edf5b00ff2ad
Alan Chandler <alan@chandlerfamily.org.uk>
2025-06-03 15:19:14 +0100

    Update to release 4.3.0

--- makerelease.sh
+++ makerelease.sh
@@ -4,4 +4,4 @@ node scripts/swupdate.js
 rsync compose-prod.yaml kanga:/volume1/docker/money/compose.yaml
 rsync -axAHX --delete appinfo/ kanga:/volume1/docker/money/appinfo/
 rsync -axAHX --delete client/ kanga:/volume1/money/client/
-rsync -axAHX --delete server/ kanga:?volume1/money/server/
+rsync -axAHX --delete server/ kanga:/volume1/money/server/
