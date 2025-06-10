20156ba9a2c9b2a9d852f56ecbbd0e2c0c8d2f8a
Alan Chandler <alan@chandlerfamily.org.uk>
2025-06-10 11:36:02 +0100

    Update for production

--- makerelease.sh
+++ makerelease.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-node scripts/swupdate.js
+rsync compose.yaml kanga:/volume1/docker/money/compose.yaml
+rsync -aq --delete appinfo/ kanga:/volume1/docker/money/appinfo/
+rsync -aq --delete client/ kanga:/volume1/money/client/
+rsync -aq --delete server/ kanga:/volume1/money/server/
 
-rsync compose-prod.yaml kanga:/volume1/docker/money/compose.yaml
-rsync -axAHX --delete appinfo/ kanga:/volume1/docker/money/appinfo/
-rsync -axAHX --delete client/ kanga:/volume1/money/client/
-rsync -axAHX --delete server/ kanga:/volume1/money/server/
+#tell server to restart
+ssh kanga "cd /volume1/docker/money; /usr/local/bin/docker container restart moneyserver"
