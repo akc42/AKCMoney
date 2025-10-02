7ea1a2fd056496c8b137e057a7432e13e9aed791
Alan Chandler <alan@chandlerfamily.org.uk>
2025-10-02 15:19:32 +0100

    Fixes #122

--- server/db-init/database.sql
+++ server/db-init/database.sql
@@ -398,21 +398,23 @@ INSERT INTO settings (name,value) VALUES('version',2), --version of this configu
 
 
 CREATE VIEW dfxaction AS
+    WITH df(name) AS (SELECT name FROM currency WHERE priority = 0 )
     SELECT t.id,t.date,t.version, src, srccode, dst, dstcode,t.description, rno, repeat,
         CASE 
-            WHEN t.currency = 'GBP' THEN t.amount
-            WHEN t.srcamount IS NOT NULL AND sa.currency = 'GBP' THEN t.srcamount
-            WHEN t.dstamount IS NOT NULL AND da.currency = 'GBP' THEN t.dstamount
+            WHEN t.currency = df.name THEN t.amount
+            WHEN t.srcamount IS NOT NULL AND sa.currency = df.name THEN t.srcamount
+            WHEN t.dstamount IS NOT NULL AND da.currency = df.name THEN t.dstamount
             ELSE CAST ((CAST (t.amount AS REAL) / currency.rate) AS INTEGER)
         END AS dfamount
     FROM
+        df,
         xaction AS t
         LEFT JOIN account AS sa ON t.src = sa.name
         LEFT JOIN account AS da ON t.dst = da.name
         LEFT JOIN currency ON 
-            t.currency != 'GBP' AND
-            (t.srcamount IS NULL OR sa.currency != 'GBP') AND
-            (t.dstamount IS NULL OR da.currency != 'GBP') AND 
+            t.currency != df.name AND
+            (t.srcamount IS NULL OR sa.currency != df.name) AND
+            (t.dstamount IS NULL OR da.currency != df.name) AND 
             t.currency = currency.name;
 
 
