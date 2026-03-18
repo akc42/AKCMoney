8241e35fa9f7867ad2516196690043b7d3a68d94
Alan Chandler <alan@chandlerfamily.org.uk>
2026-03-18 17:30:57 +0000

    Tidy Up main app css

--- client/pages/main-app.js
+++ client/pages/main-app.js
@@ -67,14 +67,7 @@ class MainApp extends LitElement {
       align-items: center;
       color: lightgrey;
 background: #0073bf;
-background: -moz-linear-gradient(top, #e2e5e6 0%, #e2e5e6 4%, #388fcb 7%, #0073bf 50%, #388fcb 100%);
-background: -webkit-gradient(left top, left bottom, color-stop(0%, #e2e5e6), color-stop(4%, #e2e5e6), color-stop(7%, #388fcb), color-stop(50%, #0073bf), color-stop(100%, #388fcb));
-background: -webkit-linear-gradient(top, #e2e5e6 0%, #e2e5e6 4%, #388fcb 7%, #0073bf 50%, #388fcb 100%);
-background: -o-linear-gradient(top, #e2e5e6 0%, #e2e5e6 4%, #388fcb 7%, #0073bf 50%, #388fcb 100%);
-background: -ms-linear-gradient(top, #e2e5e6 0%, #e2e5e6 4%, #388fcb 7%, #0073bf 50%, #388fcb 100%);
 background: linear-gradient(to bottom, #e2e5e6 0%, #e2e5e6 4%, #388fcb 7%, #0073bf 50%, #388fcb 100%);
--webkit-box-shadow: 0px -5px 31px 4px var(--shadow-color);;
--moz-box-shadow: 0px -5px 31px 4px var(--shadow-color);
 box-shadow: 0px -5px 31px 4px var(--shadow-color);
     }
     section {
@@ -89,8 +82,6 @@ box-shadow: 0px -5px 31px 4px var(--shadow-color);
       color: var(--button-text-color);
       border: none;
       border-radius: 5px;
-      -webkit-box-shadow: 0px 3px 20px 2px var(--shadow-color);
-      -moz-box-shadow: 0px 3px 20px 2px var(--shadow-color);
       box-shadow: 0px 3px 20px 2px var(--shadow-color);
       cursor: pointer;
     }
@@ -227,14 +218,8 @@ box-shadow: 0px -5px 31px 4px var(--shadow-color);
       }
       header {
 
-background: -moz-linear-gradient(top,  #388fcb 0%, #0073bf 50%, #388fcb 93%, #e2e5e6 96%, #e2e5e6 100%);
-background: -webkit-gradient(left top, left bottom, color-stop(0%, #388fcb), color-stop(50%, #0073bf), color-stop(93%, #388fcb), color-stop(96%, #e2e5e6), color-stop(100%, #e2e5e6));
-background: -webkit-linear-gradient(top,#388fcb 0%, #0073bf 50%, #388fcb 93%, #e2e5e6 96%, #e2e5e6 100%);
-background: -o-linear-gradient(top,#388fcb 0%, #0073bf 50%, #388fcb 93%, #e2e5e6 96%, #e2e5e6 100%);
-background: -ms-linear-gradient(top,#388fcb 0%, #0073bf 50%, #388fcb 93%, #e2e5e6 96%, #e2e5e6 100%);
+
 background: linear-gradient(to bottom,#388fcb 0%, #0073bf 50%, #388fcb 93%, #e2e5e6 96%, #e2e5e6 100%);
--webkit-box-shadow: -1px 5px 31px 4px var(--shadow-color);;
--moz-box-shadow: 0px 5px 31px 4px var(--shadow-color);
 box-shadow: 0px 5px 31px 4px var(--shadow-color);
       }
       #currentdomain {
