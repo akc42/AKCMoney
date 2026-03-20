1b47541d1b74da59163fd8ad07e3fa83e178fd03
Alan Chandler <alan@chandlerfamily.org.uk>
2026-03-20 15:34:59 +0000

    Update release info

--- makerelease.sh
+++ makerelease.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+node scripts/swupdate.js
+
+
 rsync compose.yaml kanga:/volume1/docker/money/compose.yaml
 rsync -aq --delete appinfo/ kanga:/volume1/docker/money/appinfo/
 rsync -aq --delete client/ kanga:/volume1/money/client/
 rsync -aq --delete server/ kanga:/volume1/money/server/
+rsync -aq --delete timer/ kanga:/volume1/money/timer/
 
 #tell server to restart
 ssh kanga "cd /volume1/docker/money; /usr/local/bin/docker container restart moneyserver"
+
+#make a git tag if it doesn't already exist
+if GIT_DIR=.git git rev-parse $(cat appinfo/version) >/dev/null 2>&1 ; then echo "tag already in use";
+else 
+ git tag $(cat appinfo/version)
+fi
+
