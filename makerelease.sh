9430e7c2700487c31e41d939cf5c25c6b6e7b90a
Alan Chandler <alan@chandlerfamily.org.uk>
2026-03-20 15:43:28 +0000

    copy environment file over

--- makerelease.sh
+++ makerelease.sh
@@ -8,7 +8,7 @@ rsync -aq --delete appinfo/ kanga:/volume1/docker/money/appinfo/
 rsync -aq --delete client/ kanga:/volume1/money/client/
 rsync -aq --delete server/ kanga:/volume1/money/server/
 rsync -aq --delete timer/ kanga:/volume1/money/timer/
-
+rsync -aq --delete common.env kanga:/volume1/docker/money/common.env
 #tell server to restart
 ssh kanga "cd /volume1/docker/money; /usr/local/bin/docker container restart moneyserver"
 
